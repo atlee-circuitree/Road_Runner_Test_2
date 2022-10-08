3a6b4546e6f31a9506e335681ea12a78f6cd153f
Viassna <maskedful@gmail.com>
2022-10-08 12:53:16 -0400

    Save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -29,11 +29,11 @@ import java.util.List;
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 8192;
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 1.4375; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 8.75; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 8.5625; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
@@ -47,11 +47,14 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
                 new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
         ));
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "leftEncoder"));
-        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "rightEncoder"));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontEncoder"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontLeft"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backLeft"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+
+            leftEncoder.setDirection(Encoder.Direction.REVERSE);
+            rightEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/Covenant_Demo.java
@@ -0,0 +1,216 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Covenant Demo", group="Linear Opmode")
+public class Covenant_Demo extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor drive_FL = null;
+    private DcMotor drive_RL = null;
+    private DcMotor drive_FR = null;
+    private DcMotor drive_RR = null;
+    private DcMotor leftArm = null;
+    private DcMotor rightArm = null;
+    private DcMotor armExtend = null;
+    private DcMotor feeder = null;
+    private Servo kickout = null;
+    private CRServo leftDucky = null;
+    private CRServo rightDucky = null;
+    private CRServo tapeArm = null;
+    private Servo odometryLift1 = null;
+    private Servo armTurn = null;
+
+    //public final static double ARM_DEFAULT = 0.5; //Unslash this if you want armTurn servo using joystick back
+    public final static double ARM_MIN_RANGE = 0.46;
+    public final static double ARM_MAX_RANGE = 0.53;
+
+    //double armTurnPosition = ARM_DEFAULT;
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        drive_FL = hardwareMap.get(DcMotor.class, "drive_FL");
+        drive_RL = hardwareMap.get(DcMotor.class, "drive_RL");
+        drive_FR = hardwareMap.get(DcMotor.class, "drive_FR");
+        drive_RR = hardwareMap.get(DcMotor.class, "drive_RR");
+        leftArm = hardwareMap.get(DcMotor.class, "left_Arm");
+        rightArm = hardwareMap.get(DcMotor.class, "right_Arm");
+        armExtend = hardwareMap.get(DcMotor.class, "extend_Arm");
+        feeder = hardwareMap.get(DcMotor.class, "feeder");
+
+
+        kickout = hardwareMap.get(Servo.class, "kickout");
+        leftDucky = hardwareMap.get(CRServo.class, "left_Ducky");
+        rightDucky = hardwareMap.get(CRServo.class, "right_Ducky");
+        tapeArm = hardwareMap.get(CRServo.class, "tapeArm");
+        odometryLift1 = hardwareMap.get(Servo.class, "odometryLift1");
+        armTurn = hardwareMap.get(Servo.class, "armTurn");
+
+        leftArm.setDirection(DcMotor.Direction.REVERSE);
+        rightArm.setDirection(DcMotor.Direction.FORWARD);
+
+        leftArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //armTurn.setPosition(ARM_DEFAULT); //Unslash this if you want armTurn servo using joystick back
+
+        //Slow Mode Variables
+
+        double SD = 1;
+        double SA = 1;
+
+        //Arm Turn Variables
+        //double armTurn = ARM_DEFAULT;  //Unslash this if you want armTurn servo using joystick back
+        final double ARM_SPEED = 0.005;
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the batter;
+        drive_FL.setDirection(DcMotor.Direction.FORWARD);
+        drive_RL.setDirection(DcMotor.Direction.FORWARD);
+        drive_FR.setDirection(DcMotor.Direction.REVERSE);
+        drive_RR.setDirection(DcMotor.Direction.REVERSE);
+
+        //Drive Modes
+        drive_FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_RL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        drive_RR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //Show encoder values on the phone
+            telemetry.addData("Status", "Initialized");
+            telemetry.addData("Left Dead Encoder", drive_FL.getCurrentPosition());
+            telemetry.addData("Right Dead Encoder", drive_RR.getCurrentPosition());
+            telemetry.addData("Rear Dead Encoder", drive_RL.getCurrentPosition());
+            telemetry.addData("Arm Encoder", rightArm.getCurrentPosition());
+            telemetry.addData("Arm Angle", rightArm.getCurrentPosition() / 20);
+            telemetry.addData("Left Arm Power", leftArm.getPower());
+            telemetry.addData("Right Arm Power", rightArm.getPower());
+            telemetry.addData("Kickout", kickout.getPosition());
+            telemetry.addData("Left Ducky Wheel", leftDucky.getPower());
+            telemetry.addData("Right Ducky Wheel", rightDucky.getPower());
+            telemetry.addData("Feeder", feeder.getCurrentPosition());
+            telemetry.addData("Arm Extend", armExtend.getCurrentPosition());
+            telemetry.addData("Arm Extend Power", armExtend.getPower());
+            telemetry.addData("Tape Arm Power", tapeArm.getPower());
+            telemetry.addData("Arm Turn Position", armTurn.getPosition());
+            //telemetry.addData("Arm Turn Position", "%.2f", armTurnPosition);
+            telemetry.update();
+
+            //Mecanum Drive Code
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = -gamepad1.right_stick_x;
+            final double v1 = r * Math.cos(robotAngle) + rightX;
+            final double v2 = r * Math.sin(robotAngle) - rightX;
+            final double v3 = r * Math.sin(robotAngle) + rightX;
+            final double v4 = r * Math.cos(robotAngle) - rightX;
+
+            drive_FL.setPower(v1 * SD);
+            drive_RL.setPower(v3 * SD);
+            drive_FR.setPower(v2 * SD);
+            drive_RR.setPower(v4 * SD);
+
+            //Controller 1
+            if (gamepad1.left_stick_button) {
+                SD = .25;
+            } else {
+                SD = 1;
+            }
+
+            //Controller 2
+            //Manually turns arm :)
+            if (gamepad1.left_trigger > .05) {
+                leftArm.setPower(gamepad1.left_trigger);
+                rightArm.setPower(gamepad1.left_trigger);
+
+            } else if (gamepad1.right_trigger > .05) {
+                leftArm.setPower(-gamepad1.right_trigger);
+                rightArm.setPower(-gamepad1.right_trigger);
+
+            } else {
+                leftArm.setPower(0);
+                rightArm.setPower(0);
+            }
+
+            if (gamepad1.b) {
+                feeder.setPower(.5); //Turns Feeder Motor Inward
+
+            } else if (gamepad1.a) {
+                feeder.setPower(-1); //Turns Feeder Motor Outward
+
+            } else { 
+                feeder.setPower(0);
+            }
+
+            //Extends and Retracts the arm
+            if (gamepad1.x) {
+                armExtend.setPower(.3);
+            } else if (gamepad1.y) {
+                armExtend.setPower(-.3);
+            } else {
+                armExtend.setPower(0);
+            }
+
+            //Turns feeder box (Higher number = left, Lower number = right)
+             if (gamepad1.dpad_left) {
+                armTurn.setPosition(.53);
+            } else if (gamepad1.dpad_right) {
+                armTurn.setPosition(.46);
+            } else if (gamepad1.dpad_up) {
+                armTurn.setPosition(.50);
+            }
+
+             //Turns feeder box variably with joystick  //Unslash this section if you want armTurn servo using joystick back.
+            /*if (gamepad2.right_stick_x > .05)         // Also slash out button armTurn , it's right above this function.
+                armTurnPosition -= ARM_SPEED;
+            else if (gamepad2.right_stick_x < -.05)
+                armTurnPosition += ARM_SPEED;
+
+            armTurnPosition = Range.clip(armTurnPosition, ARM_MIN_RANGE, ARM_MAX_RANGE);
+            armTurn.setPosition(armTurnPosition); */
+
+        }
+    }
+
+    public int degreesBore(int input) {
+
+        final int COUNTS_PER_BORE_MOTOR_REV = 8192;    // eg: GOBUILDA Motor Encoder
+        int COUNTS_TICKS_PER_REV_PER_DEGREE = (COUNTS_PER_BORE_MOTOR_REV) / 360 * 2;
+
+        return COUNTS_TICKS_PER_REV_PER_DEGREE * input;
+
+    }
+}
