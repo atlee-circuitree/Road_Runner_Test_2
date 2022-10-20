81612ab0c632fd1838e8c49d285bfe8f3aa9418c
Bushsbakedbean <114026987+Bushsbakedbean@users.noreply.github.com>
2022-10-20 18:06:04 -0400

    save

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestPath.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@Autonomous(name ="TestPath", group = "drive")
+public class TestPath extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(90));
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                .splineTo(new Vector2d(0, 40), Math.toRadians(0))
+                .build();
+
+        drive.followTrajectory(traj1);
+    }
+}
